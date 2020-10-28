ae99ff5693a5dee5d81a9d1bcd9903ab506cef4d
Thanarat Tonpradit <thanarat@dollarsign.co>
2020-10-28 17:47:17 +0700

    refactor: change nuxtjs to vuejs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
     node: true,
   },
   extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
+    'plugin:vue/essential',
+    '@vue/airbnb',
+    '@vue/typescript/recommended',
   ],
-  plugins: ['prettier'],
-  // add your custom rules here
-  rules: {},
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
 };
