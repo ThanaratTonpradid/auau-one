9ca4b607a97def72aab0d67bfc8a9ba73e1a4719
Thanarat Tonpradit <thanarat@dollarsign.co>
2020-11-02 13:43:53 +0700

    refactor: edit navbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <b-navbar toggleable="lg" type="dark" variant="primary">
-    <b-navbar-brand class="text-uppercase" @click="goHome">auau.one</b-navbar-brand>
+    <b-navbar-brand class="text-uppercase" href="/">auau.one</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
@@ -17,12 +17,6 @@ import { Component } from 'vue-property-decorator';
 
 @Component
 export default class Navbar extends Vue {
-  goHome() {
-    this.$router.push({
-      name: 'home',
-    });
-  }
-
   goAbout() {
     this.$router.push({
       name: 'about',
